bffe92128a9b66c39ad1a099924d7b03627524e6
hone5t <hone5t@hone5t.com>
2019-07-02 11:24:28 -0500

    add new user models to handle default users

--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +112,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
